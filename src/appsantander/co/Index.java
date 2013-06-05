cbffff796d855ea06e04ca038f8ba528835dca17
Mauricio Acevedo Rueda <maccevedor@gmail.com>
2013-06-04 19:24:10 -0500

    CAmbios de Ejemplo Lsita expandible
    Se crearon archivos nuevos , debido a  problemas de sincronizacion de
    dropbox

--- src/appsantander/co/Index.java
+++ src/appsantander/co/Index.java
@@ -2,24 +2,60 @@ package appsantander.co;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Index extends Activity {
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+public class Index extends Activity implements OnItemSelectedListener {
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
         
-       
-    }
-
-	
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.index, menu);
-        return true;
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+        
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Automobile");
+        categories.add("Business Services");
+        categories.add("Computers");
+        categories.add("Education");
+        categories.add("Personal");
+        categories.add("Travel");
+        
+        // Creating adapter for spinner
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+		
+		// Drop down layout style - list view with radio button
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		// attaching data adapter to spinner
+		spinner.setAdapter(dataAdapter);
     }
     
+    @Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+		// On selecting a spinner item
+		String item = parent.getItemAtPosition(position).toString();
+		
+		// Showing selected spinner item
+		Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+	}
+
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
