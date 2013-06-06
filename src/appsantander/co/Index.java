b31947b9ed7a0c2c68fa989c546e00512831fedf
Mauricio Acevedo Rueda <maccevedor@gmail.com>
2013-06-06 18:55:58 -0500

    CheckBox Funcionando
    Ya se bloquea correctamente

--- src/appsantander/co/Index.java
+++ src/appsantander/co/Index.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,9 +21,9 @@ import android.view.View.OnClickListener;
 
 public class Index extends Activity implements OnItemSelectedListener {
 	
-	 private CheckBox checkBox1;
-	 private Button button1;
-	 private EditText EditText2;
+	private CheckBox checkBox1;
+	private Button button1;
+	private EditText editText2;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +32,11 @@ public class Index extends Activity implements OnItemSelectedListener {
         
         // Spinner element
         
-    	addListenerOnChkIos();
+    	addListenerOnCheckBox();
+    	
+    	// LLama a una clase que se encarga de verificar el Checkbox
+    	
+    	
     	
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         
@@ -73,7 +78,7 @@ public class Index extends Activity implements OnItemSelectedListener {
 		
 	}
 	
-	public void addListenerOnChkIos() {
+	public void addListenerOnCheckBox() {
 		 
 		checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
 	 
@@ -84,8 +89,37 @@ public class Index extends Activity implements OnItemSelectedListener {
 	                //is chkIos checked?
 			if (((CheckBox) v).isChecked()) {
 				Toast.makeText(Index.this,
-			 	   "Hola selecciones el checBox:)", Toast.LENGTH_LONG).show();
-				
+			 	   "Hola selecciones el checBox :(", Toast.LENGTH_LONG).show();
+				// editText2.setEnabled(false);
+				EditText a = (EditText) findViewById(R.id.editText2);
+				  a.setText("Anónimo Wamberto");
+		          a.setEnabled(false);
+		          EditText b = (EditText) findViewById(R.id.editText3);
+		          b.setText("Anónimo");
+		          b.setEnabled(false);
+		          EditText c = (EditText) findViewById(R.id.editText4);
+		          c.setText("Anónimo");
+		          c.setEnabled(false);
+		          EditText d = (EditText) findViewById(R.id.editText5);
+		          d.setText("Anónimo");
+		          d.setEnabled(false);
+			}
+			else
+			{
+				Toast.makeText(Index.this,
+					 	   "Has deselecciones el checBox :)", Toast.LENGTH_LONG).show();
+				  EditText a = (EditText) findViewById(R.id.editText2);
+				  a.setText("");
+		          a.setEnabled(true);
+		          EditText b = (EditText) findViewById(R.id.editText3);
+		          b.setText("");
+		          b.setEnabled(true);
+		          EditText c = (EditText) findViewById(R.id.editText4);
+		          c.setText("");
+		          c.setEnabled(true);
+		          EditText d = (EditText) findViewById(R.id.editText5);
+		          d.setText("");
+		          d.setEnabled(true);
 			}
 		  }
 		  
