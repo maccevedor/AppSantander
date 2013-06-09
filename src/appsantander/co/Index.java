a408ab7abff360f49b0750339ed5c8b6594d6bc0
Mauricio Acevedo Rueda <maccevedor@gmail.com>
2013-06-08 19:24:37 -0500

    Problemas con los archivose
    Genero Problemas y los archivos se dañaron , debido a esto se debio
    hacer un uncommitted

--- src/appsantander/co/Index.java
+++ src/appsantander/co/Index.java
@@ -24,6 +24,9 @@ public class Index extends Activity implements OnItemSelectedListener {
 	private CheckBox checkBox1;
 	private Button button1;
 	private EditText editText2;
+	Button btnShowLocation;
+	// GPSTracker class
+	GPSTracker gps;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -123,8 +126,35 @@ public class Index extends Activity implements OnItemSelectedListener {
 		  }
 		  
 		});
+		
+	    // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {		
+				// create class object
+		        gps = new GPSTracker(AndroidGPSTrackingActivity.this);
+
+				// check if GPS enabled		
+		        if(gps.canGetLocation()){
+		        	
+		        	double latitude = gps.getLatitude();
+		        	double longitude = gps.getLongitude();
+		        	
+		        	// \n is for new line
+		        	Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();	
+		        }else{
+		        	// can't get location
+		        	// GPS or Network is not enabled
+		        	// Ask user to enable GPS/network in settings
+		        	gps.showSettingsAlert();
+		        }
+				
+			}
+		});
 	 
 	  }
+	
 	 
 	  
 	
--- src/appsantander/co/anonimo.java
+++
@@ -1,5 +0,0 @@
-package appsantander.co;
-
-public class anonimo {
-
-}
